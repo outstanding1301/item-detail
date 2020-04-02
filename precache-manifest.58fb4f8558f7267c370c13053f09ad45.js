103b2eb358b1d2421a84c725989818fea30493c5
outstandingboy <outstandingboy@kakao.com>
2020-04-02 16:36:04 +0900

    Updates

---
+++ precache-manifest.58fb4f8558f7267c370c13053f09ad45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb1475ac763961ab20fa0db74dcbf5f",
+    "url": "/item-detail/index.html"
+  },
+  {
+    "revision": "be48d680c1d0af6b9a11",
+    "url": "/item-detail/static/css/main.727f7e06.chunk.css"
+  },
+  {
+    "revision": "1c14b8a37b9c6740b37c",
+    "url": "/item-detail/static/js/2.1e655e34.chunk.js"
+  },
+  {
+    "revision": "462f991c1d2ef16102b314f225132ac4",
+    "url": "/item-detail/static/js/2.1e655e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be48d680c1d0af6b9a11",
+    "url": "/item-detail/static/js/main.0b726b90.chunk.js"
+  },
+  {
+    "revision": "a913f7924793d3f6570b",
+    "url": "/item-detail/static/js/runtime-main.a3ed6c72.js"
+  }
+]);
